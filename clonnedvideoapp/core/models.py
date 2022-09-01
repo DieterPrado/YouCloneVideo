8db346650153cb8abb29f273a3a2e5e3873bd83f
Dieter Prado <dayterp@gmail.com>
2022-08-31 19:04:21 -0500

    models created: user, channel, video, comment

--- clonnedvideoapp/core/models.py
+++ clonnedvideoapp/core/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True, )
+    user_email = models.EmailField(unique=True)
+    user_name = models.CharField(max_length=50)
+    user_last_name = models.CharField(max_length=50)
+    user_birth_date = models.DateField("Users birth date")
+    user_register_date = models.DateTimeField("Date user registered")
+    user_last_joined_date = models.DateTimeField("Last time joined")
+
+    def __str__(self):
+        return self.username
+
+
+class Channel(models.Model):
+    channel_name = models.CharField(max_length=50, unique=True)
+    channel_creation_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.channel_name
+
+
+class Video(models.Model):
+    video_file = models.FileField(upload_to="")
+    video_name = models.CharField(max_length=200)
+    video_description = models.TextField()
+    user_author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE)
+    channel_author = models.ForeignKey(
+        Channel,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.video_name
+
+class Comment(models.Model):
+    comment_text = models.TextField(max_length=1000)
+    comment_post_date = models.DateTimeField()
+    comment_is_edited = models.BooleanField()
+    comment_likes = models.IntegerField()
+    comment_dislikes = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    reply = models.ForeignKey("self",on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return "This is a comment"
+
+
+
+
+
--- clonnedvideoapp/core/tests.py
+++ clonnedvideoapp/core/tests.py
@@ -1,3 +1,35 @@
+import email
+from unicodedata import name
 from django.test import TestCase
+from django.utils import timezone
+from .models import User, Video
 
-# Create your tests here.
+def create_a_user(
+    username = "johnnyexampleAwesome", 
+    email = "john@example.com",
+    name = "Johnny",
+    last_name = "Example",
+    ):
+    return User.objects.create(username = username,
+    email = email,
+    name = name,
+    last_name = last_name
+    )
+    
+
+class UserModelTest(TestCase):
+    def test_no_user_without_username_or_email(self):
+        pass
+
+    def test_no_more_than_one_user_with_same_email(self):
+        pass
+
+    def test_no_more_than_one_user_with_same_username(self):
+        pass
+
+
+class UserDetailViewTest(TestCase):
+    pass
+
+
+
